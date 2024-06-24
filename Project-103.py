eb6fe16a2fec222ab3f014367eb0325046f6e349
ShauryaNegi2008 <73952159+ShauryaNegi2008@users.noreply.github.com>
2024-06-24 20:28:49 +0530

    Add files via upload

---
+++ Project-103.py
@@ -0,0 +1,58 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# from_dir = "ENTER THE PATH OF DOWNLOAD FOLDER (USE " / ") in VSC"
+# to_dir = "ENTER THE PATH OF DESTINATION FOLDER(USE " / ") in VSC"
+
+from_dir = "C:/Users/negis/Downloads"
+to_dir = "C:/Users/negis/Desktop/Downloaded_Files"
+
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+# Event Hanlder Class
+
+class FileMovementHandler(FileSystemEventHandler):
+    
+    
+
+    def on_created(self, event):
+     for key,value in dir_tree.items():
+      name,ext=os.path.splitext(event.src_path)
+      if ext in value:
+        file_name=os.path.basename(event.src_path)
+        print("Hi "+file_name+" To Downloads")
+    
+    def on_moved(self,event):
+       print("Oops "+event.src_path+" Someone moved your file")
+
+# Initialize Event Handler Class
+event_handler = FileMovementHandler()
+
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
+
+
+while True:
+    time.sleep(2)
+
+    
